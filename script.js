b807b712efa2cb1d894920c3e3bf0bd3daa2aaf3
Baktygul Kozhoshova <baktygul.kozhoshova.kk@gmail.com>
2021-10-11 17:02:51 +0600

    if else

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ prompt(`What is their name?`);
 
 let n=Math.random()*100;
 n=Math.floor(n)+1;
-alert(`Your love score is `+n);
+
+if(n>78){
+    alert(`Your love score is `+n +`% . You love each other like kanye loves Kiki`);
+}else{
+    alert(`Your love score is `+n+`%`);
+}
+
+
