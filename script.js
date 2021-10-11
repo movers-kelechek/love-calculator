6d2f47408aaa4d2329fa67bb75384dbaa17558f7
Baktygul Kozhoshova <baktygul.kozhoshova.kk@gmail.com>
2021-10-11 17:10:19 +0600

    comparator

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ prompt(`What is their name?`);
 let n=Math.random()*100;
 n=Math.floor(n)+1;
 
-if(n>78){
+if(n>70){
     alert(`Your love score is `+n +`% . You love each other like kanye loves Kiki`);
-}else{
-    alert(`Your love score is `+n+`%`);
+}
+if(n>30 && n<=70){
+    alert(`Your love score is `+n+`%. `);
+}
+if(n<=30){
+    alert(`Your love score is `+n+`%. You go together like oil and water`);
 }
 
 
